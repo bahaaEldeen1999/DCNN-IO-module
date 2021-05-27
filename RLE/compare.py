8b06ae4f5d0cabee143ef8669dcfec86dc118215
Dina Alaa <d.alaa@studio50s.com>
2021-05-27 17:12:40 +0200

    python

--- RLE/compare.py
+++ RLE/compare.py
@@ -1,14 +1,16 @@
 f = open("compressOutput.txt", "r")
 f2 = open("memOutput.txt", "r")
-startingOffset=16
+startingOffset=14
 imageSize=1024
 for i in range(startingOffset):
     f2.readline()
-x=["bin_filtersconv2d_1.txt","bin_biasesconv2d_1.txt",
-   "bin_filtersconv2d_2.txt","bin_biasesconv2d_2.txt",
-   "bin_filtersconv2d_3.txt","bin_biasesconv2d_3.txt",
-    "bin_biasesdense_1.txt","bin_weightsdense_1.txt",
-    "bin_biasesdense_2.txt","bin_weightsdense_2.txt"]
+x=[
+    "../modules/parameters/bin_filtersconv2d_1.txt","../modules/parameters/bin_biasesconv2d_1.txt",
+    "../modules/parameters/bin_filtersconv2d_2.txt","../modules/parameters/bin_biasesconv2d_2.txt",
+   "../modules/parameters/bin_filtersconv2d_3.txt","../modules/parameters/bin_biasesconv2d_3.txt",
+    "../modules/parameters/bin_biasesdense_1.txt","../modules/parameters/bin_weightsdense_1.txt",
+    "../modules/parameters/bin_biasesdense_2.txt","../modules/parameters/bin_weightsdense_2.txt"
+]
 fres = open("result.txt", "w")
 
 for k in x:
@@ -17,13 +19,9 @@ for k in x:
             lin2=line.strip('\n')
             newLine=f2.readline()
             number_bin = (bin(int(newLine.strip()))[2:]) 
-            number_bin = number_bin.zfill(8)
-            newLine=f2.readline()
-            number_bin2 = (bin(int(newLine.strip()))[2:]) 
-            number_bin2 = number_bin2.zfill(8)
-            if(lin2!=(number_bin+number_bin2)):
-                print(False)
-        
+            number_bin= number_bin.zfill(16)
+            if(lin2!=(number_bin)):
+               print(False)
 for i in range(imageSize):
     a=f.readline().strip()
     b=f2.readline().strip()
